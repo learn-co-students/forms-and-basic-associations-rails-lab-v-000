878632e25db4c41bb1ad175943f87136a6336436
Ashi Krishnan <me@ashi.works>
2016-01-12 13:31:55 -0500

    Artist name specs pass.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      self.notes.build(content: content)
+    end
+  end
 end
