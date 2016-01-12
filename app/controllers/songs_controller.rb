5c84ad49c8f27e9d4dfd57471cc3caafa45ebfdf
Ashi Krishnan <me@ashi.works>
2016-01-12 13:02:54 -0500

    Songs controller can create songs.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,37 +11,13 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
-  def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
-    end
-  end
-
   def edit
     @song = Song.find(params[:id])
   end
 
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
+  def create
+    song = Song.create(song_params)
+    redirect_to controller: 'songs', action: 'index'
   end
 
   private
@@ -50,4 +26,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title)
   end
 end
-
