ad65b823167874a6140c71bd186e71db51327b8f
Ashi Krishnan <me@ashi.works>
2016-01-12 12:54:08 -0500

    Song creation.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,12 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
 end
--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -4,6 +4,6 @@ require 'capybara/rspec'
 describe "the song form", :type => :feature do
   it "has a title field" do
     visit '/songs/new'
-    expect(page).to have_text_field name: 'song[title]'
+    expect(page).to have_selector 'input[name="song[title]"]'
   end
 end
