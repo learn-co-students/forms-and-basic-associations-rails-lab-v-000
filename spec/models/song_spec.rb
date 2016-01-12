b38717032380789d6a151ebfdd2a9f8331626aaf
Ashi Krishnan <me@ashi.works>
2016-01-12 13:21:42 -0500

    Song model spec for bulk methods

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'genre_name=' do
+    it 'sets the genre to the genre of the specified name' do
+      song = Song.new
+      song.genre_name = 'Rock'
+      expect(song.genre.name).to eq 'Rock'
+    end
+  end
+
+  describe 'note_contents=' do
+    it 'sets notes for a song' do
+      song = Song.new
+      notes = ['Great song', 'much bass', 'wow']
+      song.note_contents = notes
+      expect(song.notes.map(&:content)).to eq notes
+    end
+    it 'adds to existing notes' do
+      song = Song.new
+      notes = ['Great song']
+      song.note_contents = ['Great song']
+      expect(song.notes.map(&:content)).to eq notes
+      notes << 'much bass'
+      song.note_contents = ['much bass']
+      expect(song.notes.map(&:content)).to eq notes
+      notes << 'wow'
+      song.note_contents = ['wow']
+      expect(song.notes.map(&:content)).to eq notes      
+    end
+  end
 end
