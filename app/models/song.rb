4191c9f6b62912d91681591ddaa09e6a3ff3ce60
jakebrady5 <jakebrady5@gmail.com>
2016-01-17 22:25:13 -0500

    almost there

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,8 +3,8 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
+  def artist=(name)
+    self.artist_id = Artist.find_or_create_by(name: name).id
   end
 
   def song_notes=(notes)
@@ -13,11 +13,16 @@ class Song < ActiveRecord::Base
     end
   end
 
+  def genre=(id)
+    self.genre_id = id.to_i
+  end
+
   def note_contents
     content = []
     self.notes.each do |note|
       content << note.content
     end
+    content
   end
 
 end
