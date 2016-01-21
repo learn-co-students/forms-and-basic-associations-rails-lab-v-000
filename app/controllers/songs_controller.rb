3629e0fb97de450e77ae738c09193aab94444e19
Lukeghenco <lukeghenco@gmail.com>
2016-01-21 16:19:57 -0500

    I got the first two test passing, but I am not getting the results I want when I pass the third test

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,9 +13,8 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
-      redirect_to @song
+      redirect_to (@song)
     else
       render :new
     end
@@ -47,7 +46,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:artist_name, :title, :genre_id)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,22 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+    self.genre_id = genre.id
+    self.save
+  end
+end
