081281242a0b1de10080cb5262dd78f85e9616af
c1505 <coreym247@gmail.com>
2016-02-18 19:00:03 -0500

    first solution

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  attr_reader :artist_name
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+ def note_contents
+  self.notes.map {|note| note.content} 
+end
+
 end
 
