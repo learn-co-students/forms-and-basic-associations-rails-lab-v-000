b9abdf0779913320a9a48c7b6dd3928e2443b1c5
febbraiod <donedesign1@gmail.com>
2016-02-22 22:58:50 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,11 +4,21 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
+
   def artist_name
-    artist.name
+  end
+
+  def notes=(notes)
+    notes.each do |note|
+      self.notes.build(content: note)
+    end
+  end
+
+  def note_contents
+    self.notes.collect {|note| note.content}
   end
 
 end
