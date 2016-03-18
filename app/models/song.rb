a489139e4a3f307c263b4b61b7d84955b37b1d2f
Joseph Surla <josephsurla@gmail.com>
2016-03-18 12:32:27 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  attr_reader :artist_name
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    notes.collect {|note| note.content}
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes.build(content: note)
+    end
+  end
+end
