049432181e35cd5cb8a79750638de08b8217b4f9
Chris Bruen <bruen.chris@gmail.com>
2016-03-20 13:53:45 -0400

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,26 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  attr_reader :artist_name
+  
+
+  
+  def artist_name=(name)
+  	self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def note_contents=(contents)
+  	contents.each do |note|
+  		if note.strip != ""
+  		self.notes << Note.find_or_create_by(content: note, song_id: self.id)
+  		end
+  	end
+  end
+
+  def note_contents
+  	self.notes.collect { |x| x.content }
+  end
+
 end
 
