b688162fb465b27079aa620f379b53c4c1900b5d
authorbeard <msnolan@gmail.com>
2016-03-22 11:15:17 -0400

    code thus far for reference. not working

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,17 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist=Artist.find_or_create_by(name: name)
+  end
+
+  def notes=(notes_array)
+byebug
+    notes_array.each {|n|
+      self.notes << Note.create(content: n.content)
+
+    }
+  end
 end
 
