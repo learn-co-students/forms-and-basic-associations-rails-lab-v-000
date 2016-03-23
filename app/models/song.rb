4ef633119be29868e50674679d9209774e9f563b
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-23 00:34:31 +0000

    adds form partial and artist_name field to form

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  def artist_name
+    self.artist.name if self.artist
+  end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
 end
 
