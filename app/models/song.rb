90d1ad7c4b4bc75d74a3c3f7cdf3516c73d2ce04
Brian Holland <brian.holland@proatria.com>
2016-03-31 14:59:24 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,26 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name
+    if artist
+      self.artist.name
+    end
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    self.notes.map{|note| note.content}
+  end
+
+  def note_contents=(array_of_notes)
+    array_of_notes.each do |one_note|
+      note = self.notes.build(content: one_note)
+      note.save
+    end
+  end
 end
 
