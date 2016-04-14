490bb5bdc29cd244dc395beb20869773e04e738f
cernanb <cernanb@gmail.com>
2016-04-13 19:25:10 -0700

    created forms with associations

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,25 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def note_contents
+    self.notes.collect {|note| note.content}
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      if note.strip != ""
+        content = self.notes.build(content: note)
+        content.save
+      end
+    end
+  end
+end
