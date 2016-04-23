be18b6c6283a3650f56346a92d8996acd91973e6
e-baker <eric@ericbaker.me>
2016-04-22 23:03:01 -0400

    finish autocomplete

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+    self.save
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
 
