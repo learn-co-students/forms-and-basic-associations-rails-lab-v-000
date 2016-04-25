7e58f3bfce9f3a93d5be8915d9556849f0c9050f
corya0687 <corya0687@gmail.com>
2016-04-25 19:29:21 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist= Artist.find_or_create_by(name:name)
+  end
+
+
+end
