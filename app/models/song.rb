046d6664264940456c42941a4df3d2665ca4a3b5
viparthasarathy <viparthasarathy@gmail.com>
2016-05-02 17:58:31 -0700

    pass model tests

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,32 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      note = Note.find_or_create_by(content: content)
+      self.notes << note if note.valid?
+    end
+  end
+
+  def note_contents
+    self.notes.map{|n| n.content}
+  end
+
+end
