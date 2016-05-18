30990a74d473af6ff6cfcac8c26fa6c64f91f12c
hudbuz <hzb2@cornell.edu>
2016-05-18 13:36:48 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,29 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.name
+
+  end
+
+
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    genre.name
+  end
+
+
+
 end
 
--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -12,8 +12,10 @@ describe "the song form", :type => :feature do
   it "creates a song with an artist" do
     visit '/songs/new'
     fill_in :song_title, with: 'Little Earthquakes'
+
     fill_in :song_artist_name, with: 'Tori Amos'
     find('input[name="commit"]').click
+    
     expect(Song.last.artist.name).to eq 'Tori Amos'
     expect(Artist.find_by(name: 'Tori Amos').songs.pluck(:title)).to include 'Little Earthquakes'
   end
