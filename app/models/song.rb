6496b6a68ba176d028311d98c2421bd6225e1346
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-23 23:39:29 +0100

    Song readers and writers

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,32 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents=(contents)
+    contents.select! {|content| !content.empty? }
+    contents.each do |content|
+      self.notes << Note.find_or_create_by(content: content)
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
 end
 
