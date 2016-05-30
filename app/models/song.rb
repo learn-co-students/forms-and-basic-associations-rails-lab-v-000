364a24eca0e2c66d80fa3cbd51a9e15c3145fa70
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-05-30 12:05:15 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,35 @@
 class Song < ActiveRecord::Base
+  attr_accessor :artist_name, :genre_name, :note_contents
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  validates :title, presence: true
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    @artist_name = self.artist.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+    @genre_name = self.genre.name
+  end
+
+  def note_contents=(notes)
+
+        notes.each do |content|
+          if content !=nil
+            if !content.empty?
+              self.notes << Note.find_or_create_by(content: content)
+            end
+          end
+        end
+
+    # binding.pry
+    @note_contents = self.notes.collect {|note| note.content}
+
+  end
+
 end
 
