c9a268a42a6dbcc443e55289dbe358c77445209b
abadfish <kirsten.ofarrell@gmail.com>
2016-07-09 09:16:36 -0400

    done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,6 +11,9 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+
+
+
   def create
     @song = Song.new(song_params)
 
@@ -47,7 +50,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
-
