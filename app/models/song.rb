d440995050f5d288adba21d008f3d590533fce66
hangloos <ericloos00@gmail.com>
2016-07-09 15:51:21 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,41 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  
+  def artist_name
+    @artist_name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    @genre_name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      if note != "" || 
+        note =  Note.find_or_create_by(content: note)
+        self.notes << note 
+      end
+    end
+   # contents.each do |content|
+     # self.notes
+   # self.notes.content = Note.find_or_create_by(content: content)
+  end
+
+  def note_contents
+    @note_contents
+  end
 end
 
+#so I need to create the setter and getter methods so that instead of calling the object of artist, I can just get the name. 
+
+#allow a category_name field to be permitted in create which calls that method that automatically saves the category to the post and or creates the category. 
