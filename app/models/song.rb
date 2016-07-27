b67d59a5eb63eb04d4840098083454d994eb9a64
Christian Whitesides <whitesidescr@gmail.com>
2016-07-26 18:05:56 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,25 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+## try(*a, &b) is defined to be able to write:
+## `@person.try(:name)`
+
+## instead of:
+## @person.name if @person
+
+## try calls can be chained:
+## @person.try(:spouse).try(:name)
+
+## instead of:
+## @person.spouse.name if @person && @person.spouse
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
     self.artist.name if self.artist
+    # self.try(:artist).try(:name)
   end
 
   def genre_name=(name)
@@ -17,12 +30,14 @@ class Song < ActiveRecord::Base
 
   def genre_name
     self.genre.name 
+    # self.try(:genre).try(:name)
   end
 
-  def note_contents=(contents)
-    contents.each do |content|
+  def note_contents=(notes)
+    notes.each do |content|
       if content.strip != ""
         self.notes << Note.create(content: content)
+        # self.notes.build(content: content)
       end
     end
   end
