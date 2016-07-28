a2e09523811863f88fdc42a12fc9c8947659749c
jbok4 <jbok4@aol.com>
2016-07-28 11:27:10 -0400

    models and views progress

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,16 +2,40 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  validates :title, presence: :true
+  #validates :title, presence: :true
 
-  def song_artist_name=(name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
 
-  def song_artist_name
-    self.artist
+  def artist_name
+    self.artist.name
+  end
+
+  def genre_name=(name)
+     self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      note = Note.create(content: content)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    @note_array = []
+    self.notes.each do |note|
+      @note_array << note.content
+    end
+    @note_array
+    end
   end
 
 
-end
+
 
