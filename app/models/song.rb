35b78fd54025340f9f76dafe853c2d84ab59a3ff
peterberwind <peterberwind@gmail.com>
2016-08-25 19:12:32 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,7 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
 
+  # Why does it need a condition?
   def artist_name
     self.artist.name if self.artist
   end
@@ -17,6 +18,22 @@ class Song < ActiveRecord::Base
 
   def genre_name
     self.genre.name if self.genre
+  end
+
+  def note_contents=(notes)
+    # cycled through notes array and if not blank, shove into notes attribuate
+    notes.each do |note|
+      if note != ""
+        self.notes << Note.find_or_create_by(content: note)
+      end
+    end
+  end
+
+  def note_contents
+    # returns a array of notes
+    self.notes.collect do |note|
+      note.content
+    end
   end
 
 end
