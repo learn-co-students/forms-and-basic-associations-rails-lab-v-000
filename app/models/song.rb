78d8ea553810ea7e5e1152ca506211a4ce6de5e1
tpstar <hanlee12@gmail.com>
2016-08-26 00:10:41 -0400

    song_spec 4 fails

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,31 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      note = Note.find_by(content: content)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    self.notes.map {|note| note.content}
+  end
+end
