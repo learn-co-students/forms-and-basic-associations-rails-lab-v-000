647e9bfcfe106b80b8b2e0470957fbeb63e61cc3
tpstar <hanlee12@gmail.com>
2016-08-27 08:14:40 -0500

    stuck at the last ex

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,7 +9,7 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name
-    self.genre.name
+    self.genre.name if self.genre
   end
 
   def artist_name=(name)
@@ -17,7 +17,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name
-    self.artist.name
+    self.artist.name if self.artist
   end
 
   def note_contents=(contents)
