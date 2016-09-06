8ff857991b3183c87da378531a93dc9387d01b3f
miclp <michaelpeimani@gmail.com>
2016-09-05 22:42:03 -0700

    Complete

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,12 +11,25 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
 
-  # def genre_name
-  #   self.genre
-  # end
-  #
-  # def genre_name=(name)
-  #   self.genre = Genre.find_or_create_by(name: name)
-  # end
+  def note_contents
+    self.notes.collect{|note| note.content}
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      if !note.empty?
+        note = Note.find_or_create_by(content: note)
+        self.notes << note
+      end
+    end
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
 
 end
