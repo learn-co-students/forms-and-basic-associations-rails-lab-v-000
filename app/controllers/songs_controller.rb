a7ff74eebd1a66bfd1490a1d4c307c55b313d9d1
Ivan Pilot <ivanpilot@gmail.com>
2016-09-27 13:22:31 +0100

    all tests passed

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,6 @@ class SongsController < ApplicationController
   end
 
   def create
-    binding.pry
     @song = Song.new(song_params)
 
     if @song.save
@@ -48,6 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :notes_1, :notes_2)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,11 +3,42 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  attr_reader :artist_name
+  attr_reader :notes_1, :notes_2
 
   def artist_name=(name)
-    # binding.pry
     self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def notes_1=(note)
+    self.notes << Note.create(content:note)
+  end
+
+  def notes_2=(note)
+    self.notes << Note.create(content:note)
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes << Note.create(content:note) if note.empty? == false
+    end
+  end
+
+  def note_contents
+    self.notes.map {|note| note.content}
   end
 
 end
