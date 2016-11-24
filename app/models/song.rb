9413013694e279c3556b106b6e52da0009419679
kkterai <kkterai@gmail.com>
2016-11-23 23:28:51 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,14 +7,29 @@ class Song < ActiveRecord::Base
     self.genre= Genre.find_or_create_by(name: name)
   end
 
+  def genre_name
+    self.genre.name
+  end
+
   def artist_name=(name)
     self.artist= Artist.find_or_create_by(name: name)
   end
 
-  def note_contents=(ids)
-    ids.each do |id|
-      note = Note.find(id)
-      self.notes << note
+  def artist_name
+    self.artist.try(:name)
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content != ""
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.collect do |note|
+      note.content
     end
   end
 end
