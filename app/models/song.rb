7a8f0d52c7ebda187a9ff3fe3dae3a9b16c870d6
David Kennell <davidkennell0@gmail.com>
2016-12-13 15:45:49 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+
+  attr_reader :song_artist_name
+
+  def song_artist_name=(name)
+    self.artist = Artist.find_by(name: name)
+  end
+
+end
