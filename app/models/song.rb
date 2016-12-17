902d6873d084456b99d192cc0f6e915d83ba475d
David Kennell <davidkennell0@gmail.com>
2016-12-17 15:01:45 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,19 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  attr_reader :song_genre_id, :song_artist_name, :note_contents
+  attr_reader :song_genre_id, :song_artist_name
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+
+  def artist_name
+    self.artist.name
+  end
 
   def song_artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
@@ -15,8 +27,18 @@ class Song < ActiveRecord::Base
 
   def note_contents=(note_contents)
     note_contents.each do |content|
-      self.notes << Note.find_or_create_by(content)
+      if content != ""
+        self.notes.build(content: content)
+      end
     end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
 end
