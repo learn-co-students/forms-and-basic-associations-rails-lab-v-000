50e1c5dc2d9062ee621f197b67110b3ae08446c6
neener <janinemrosen@gmail.com>
2017-01-04 15:23:40 -0500

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name
-  	self.genre.name
+  	self.genre.name if self.genre
   end
 
   def artist_name=(name)
@@ -16,7 +16,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name
-  	self.artist.name
+  	self.artist.name if self.artist
   end
 
   def note_contents
