e3dfe88e324adbc44192bed416b94f315c70a3da
WilliamMena <williammena1991@gmail.com>
2017-01-18 20:49:56 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+  def artist_name
+    if self.artist != nil
+      self.artist.name
+    end
+  end
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
