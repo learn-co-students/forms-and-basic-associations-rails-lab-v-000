67e20ded8dc9da2c247751d7fdcb1eca7af76867
WilliamMena <williammena1991@gmail.com>
2017-01-18 23:55:00 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,5 +12,40 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def genre_name
+    if self.genre != nil
+      self.genre.name
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    if self.notes != nil
+      content_array = []
+      self.notes.each do |note|
+        content_array << note.content
+      end
+    end
+    content_array
+  end
+
+  def note_contents=(notes)
+
+    notes.each do |note|
+      if !note.blank?
+        actual_note = Note.create(content: note)
+        self.notes << actual_note
+      end
+    end
+  end
+
+
+
+
+
 end
 
