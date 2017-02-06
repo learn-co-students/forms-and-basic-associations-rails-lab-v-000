240444c7babdc63e3af22ebf2429ab01052a73dd
Adam Lucia <lucia.adam@gmail.com>
2017-02-06 07:58:44 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,49 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(gname)
+    self.genre = Genre.find_or_create_by(name: gname)
+    self.save
+  end
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    else
+      nil
+    end
+  end
+
+  def artist_name=(aname)
+    self.artist = Artist.find_or_create_by(name: aname)
+    self.save
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def note_contents=(words)
+    words.each do |word|
+      if word != ""
+        self.notes << Note.find_or_create_by(content: word)
+      end
+    end
+    self.save
+  end
+
+  def note_contents
+    contents = []
+    self.notes.each do |note|
+      contents << note.content
+    end
+    contents
+  end
+end
