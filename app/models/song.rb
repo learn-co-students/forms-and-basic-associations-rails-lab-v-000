50f036130a9c218f8583d3cb47a09c3e3df4a693
Tayari Appiah <cjhhh.ta@gmail.com>
2017-02-11 20:48:40 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,29 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist
+  end
+
+  def note_contents=(content)
+    binding.pry
+      notes.each do |content|
+      self.notes.build(:content => content)
+    end
+  end
+
+  def note_contents
+    notes.each do |content|
+      content
+    end
+  end
+
+
+end
