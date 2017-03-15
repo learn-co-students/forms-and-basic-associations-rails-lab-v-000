9bda6839010064b08aead497a317cdfae205e825
Kevin Adkins <kevinladkins@gmail.com>
2017-03-15 18:57:30 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,16 +4,19 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
+      self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
-
+     artist.name
   end
 
   def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
+    genre.name
   end
 
   def note_contents=(notes)
