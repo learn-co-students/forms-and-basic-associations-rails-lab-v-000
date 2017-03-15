888e3dd3c4001858375425c3c20e732eb95ffe45
Kevin Adkins <kevinladkins@gmail.com>
2017-03-15 18:55:46 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,24 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+  end
+
+  def artist_name
+
+  end
+
+  def genre_name=(name)
+  end
+
+  def genre_name
+  end
+
+  def note_contents=(notes)
+  end
+
+  def note_contents
+  end
 end
 
