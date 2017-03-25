4e1a0ca556560d74039295280f1efaa40517ef72
John Pfingst <jpfingst@outlook.com>
2017-03-25 18:21:21 +0000

    new song partially working

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
-  has_many :notes
-end
+  has_many   :notes
 
+  validates :title, presence: true
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if !artist.nil?
+  end
+end
