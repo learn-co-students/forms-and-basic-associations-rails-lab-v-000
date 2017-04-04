e23186397e698ea1d06e121fca0820f4cabec7d9
Hui Jiang <hui1021@yahoo.com>
2017-04-04 22:54:13 +0000

    updated models

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,32 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name= (name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def artist_name= (name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def note_contents= (notes)
+    notes.each {|content| self.notes.build(content: content) if content != "" }
+  end
+
+  def note_contents
+    self.notes.collect {|note| note.content}
+  end
+
+
+end
