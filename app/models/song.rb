a69e650b2a7a385026205b469870d25db559f937
bentonwong <bentonwong@gmail.com>
2017-04-13 12:43:43 -0700

    add view and modify model and song_params

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,29 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def note_contents
+    self.notes.map{|note| note.content} if self.notes
+  end
+
+  def note_contents=(notes)
+    notes.each {|note| self.notes << Note.create(content: note) if !note.blank?}
+  end
+
+end
