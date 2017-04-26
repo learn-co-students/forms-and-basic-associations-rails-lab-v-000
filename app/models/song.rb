ae4c8a33f26c0a5e304227bd8cf79f322385b681
Howard DeVennish <howarddevennish@gmail.com>
2017-04-26 15:23:53 +0000

    add genre and artist getters and setters to song

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,20 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name
+    self.try(:artist).try(:name)
   end
 
-  def artist_name=
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
   end
 end
