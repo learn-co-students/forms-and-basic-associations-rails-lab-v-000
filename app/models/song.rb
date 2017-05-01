71f6b5c5c73a0ae6fa2e4b2fa4d61e4a27f5ffcf
github username <github email address>
2017-05-01 10:15:46 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
-  	self.artist = Artist.find_or_create_by(name: name)
+  	artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
