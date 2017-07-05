85d0e5821eb5eea8ea042f805522a16241616aa4
WilliamBarela <william.barela.dev@gmail.com>
2017-07-05 16:34:02 +0900

    Done.

--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -14,7 +14,6 @@ describe "the song form", :type => :feature do
     fill_in :song_title, with: 'Little Earthquakes'
     fill_in :song_artist_name, with: 'Tori Amos'
     find('input[name="commit"]').click
-    binding.pry
     expect(Song.last.artist.name).to eq 'Tori Amos'
     expect(Artist.find_by(name: 'Tori Amos').songs.pluck(:title)).to include 'Little Earthquakes'
   end
