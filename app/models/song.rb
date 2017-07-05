ff4da3a787d9467e12d6952e35f4bd09a616fc1b
WilliamBarela <william.barela.dev@gmail.com>
2017-07-05 15:11:02 +0900

    added #artist_name and #song_genre

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(:name => name)
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(:name => name)
+  end
+
+  def artist_name
+    self.try(:artist).try(:name) 
+  end
+
+  #  note_contents
+  #    returns the content of all notes as an array (FAILED - 6)
+  #  note_contents=
+  #    sets notes for a song (FAILED - 7)
+  #    adds to existing notes (FAILED - 8)
+  #    ignores blank notes (FAILED - 9)
+
+  def note_contents=(content)
+  end
+  
+  def note_contents
+  end
+end
