4cd015130426090ab3b0c0f7cf0ae98cd3bf5a85
James Dolan <jwdolan820@gmail.com>
2017-07-05 03:14:19 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114013240) do
+ActiveRecord::Schema.define(version: 20170114013240) do
 
-  create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "artists", force: :cascade do |t|
+   t.string   "name"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "genres", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "genres", force: :cascade do |t|
+   t.string   "name"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "notes", force: :cascade do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "song_id"
-  end
+ create_table "notes", force: :cascade do |t|
+   t.string   "content"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+   t.integer  "song_id"
+ end
 
-  create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "artist_id"
-    t.integer  "genre_id"
-  end
+ add_index "notes", ["song_id"], name: "index_notes_on_song_id"
+
+ create_table "songs", force: :cascade do |t|
+   t.string   "title"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+   t.integer  "artist_id"
+   t.integer  "genre_id"
+ end
+
+ add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
+ add_index "songs", ["genre_id"], name: "index_songs_on_genre_id"
 
 end
