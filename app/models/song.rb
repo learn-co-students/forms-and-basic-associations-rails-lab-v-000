a31337f828d9e3c235a54ee2e55ef6f8fb54fa03
Samson Yuwono <samson.yuwono@gmail.com>
2017-07-24 16:11:50 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,21 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(artist_name)
+    self.artist= Artist.find_or_create_by(name: artist_name)
+  end
+
+  def artist_name #reader
+    self.artist.name
+  end
+
+  def genre_name=(genre_name)
+    self.genre= Genre.find_or_create_by(name: genre_name)
+  end
+
+  def genre_name #reader
+    self.genre.name
+  end
+
+end
