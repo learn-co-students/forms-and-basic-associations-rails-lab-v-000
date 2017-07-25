d90a29210257b4dd615eb46199bb4c4ca9d7e8d4
Pamela Bortnick <pbortnick@gmail.com>
2017-07-25 17:00:30 +0000

    Done.

---
+++ db/migrate/20170724210732_create_add_artist_name_to_songs.rb
@@ -0,0 +1,5 @@
+class CreateAddArtistNameToSongs < ActiveRecord::Migration
+  def change
+    add_column :songs, :artist_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114013240) do
+ActiveRecord::Schema.define(version: 20170725162940) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -34,10 +34,13 @@ ActiveRecord::Schema.define(version: 20160114013240) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "artist_id"
     t.integer  "genre_id"
+    t.string   "artist_name"
+    t.string   "note_contents"
+    t.string   "genre_name"
   end
 
 end
