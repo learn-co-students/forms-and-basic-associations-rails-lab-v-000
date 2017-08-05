648d84c8de9b2185fab7ce05dcdeddd92222516f
blinded93 <blinded93@gmail.com>
2017-08-05 08:59:17 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,7 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    set_artist
   end
 
   def new
@@ -22,11 +22,11 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
+    set_artist
   end
 
   def update
-    @artist = Artist.find(params[:id])
+    set_artist
 
     @artist.update(artist_params)
 
@@ -38,7 +38,7 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
+    set_artist
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
@@ -48,5 +48,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def set_artist
+    @artist = Artist.find_by(id: params[:id])
   end
 end
