3a1578f0a3b5d69b10a3473af240ff557ff020bf
Andrew <github email address>
2017-08-21 22:33:13 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,17 +9,28 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
   def artist_name
-    self.artist.name if self.artist != nil
+    self.artist.name if self.artist 
   end
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
   def genre_name
-    self.genre.name if self.genre != nil
+    self.genre.name if self.genre
   end
 
-  def note_ids
-    self.note.id
+  def note_contents=(notes)
+  
+      notes.each do |content|
+      if content != ""
+      self.notes.build(content: content)
+      
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  
   end
 end
 
