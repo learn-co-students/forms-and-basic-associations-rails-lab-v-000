4d0b122ee379464bb725624e784863e2474be06c
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-23 19:09:55 +0000

    added artist to the form

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(name:artist)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+end
