04ec5f37d65e45330030d9d5ecd6329ef56e1cba
streamedLine <dovidlwtn@gmail.com>
2017-08-30 23:22:07 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,7 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
   end
 
   def create
@@ -47,7 +48,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id)
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,29 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(name)
+  	self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name 
+  	self.genre.name 
+  end
+
+  def artist_name=(name)
+  	self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+  	self.artist.name if self.artist
+  end
+
+  def note_contents=(notes)
+  	notes.each{|note| self.notes << Note.create(content: note) unless note == ''}
+  end
+
+  def note_contents
+  	self.notes.map(&:content)
+  end
 end
 
