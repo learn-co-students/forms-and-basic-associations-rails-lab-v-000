20836497869f8ea247ce9c865b5c1c27680e1758
emhopkins <epoletto@lstreetc.com>
2017-09-06 01:06:43 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,9 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.artist = Artist.new
+    @song.genre = Genre.new
+    @song.notes = []
   end
 
   def create
@@ -47,7 +50,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_name, note_contents: [])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,38 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name
+  	self.artist.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_name
+  	self.genre.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def note_contents
+  	notes_content_array = []
+  	self.notes.each do |note|
+  		notes_content_array << note.content 
+  	end
+  	notes_content_array
+  end
+
+  def note_contents=(contents)
+  	contents.each do |content|
+  	  if !content.empty?
+      	note = Note.find_or_create_by(content: content)
+	  	self.notes << note
+	  end
+    end
+  end
 end
 
