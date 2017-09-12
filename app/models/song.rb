0d1e3d524626826ad36526f9f1cce19e15dd9de2
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-12 15:32:36 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def song_artist_name
+  def artist_name
     self.try(:artist).try(:name)
   end
 # .try is look for an object, and return nil if it doesn't detect it instead of error
 
-  def song_artist_name=(name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
 
