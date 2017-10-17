003c060c843de864c1bc00e9dcf0c16a1ae38a95
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-17 22:28:01 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,32 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name if self.genre
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def note_contents=(arr)
+    arr.each do |content|
+      self.notes.build(content: content) unless content.empty?
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+end
