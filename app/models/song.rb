bc1f11f4a108f73c542d211775c4b81e61475b1e
github username <github email address>
2017-11-21 15:58:30 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,39 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist
+  belongs_to :artist # a song instance belongs to an artist instance
   belongs_to :genre
   has_many :notes
-end
+# custom setter #artist_name=(name) is called whenever a song instance is initialized w/ artist_name field (for artist_name attribute)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
 
+  def artist_name
+    self.try(:artist).try(:name)
+    # above code is the same as:
+    # if self.artist
+    #  self.artist.name
+    # end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def note_contents=(notes) # notes argument is an array of strings, and each string is possible content attribute value of a note instance
+    notes.each do |content|
+      if !content.empty?
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+    # above code is same as saying
+    # self.notes.map {|note| note.content}
+  end
+end
