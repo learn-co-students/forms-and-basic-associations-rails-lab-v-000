d4edc0de1c1720c76d41e9b9350534d8999562e1
Deborah Seow <deborahseow9@gmail.com>
2017-11-25 05:57:12 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,18 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+attr_reader :artist_name, :genre_name, :note_contents
+
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(artist)
+  end
+
+  def genre_name=(genre)
+    self.genre = Genre.find_or_create_by(genre)
+  end
+
+  def note_contents=(contents)
+
+  end
+end
