bc42051c3a9a7b4cfde34ecddcfc584a0ccec80a
kyleblee <hello.kylelee@gmail.com>
2017-11-28 22:08:30 -0500

    lab complete

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name
+    unless self.artist == nil
+      self.artist.name
+    end
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    unless self.notes.empty?
+      self.notes.collect {|note| note.content}
+    end
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes.build(content: note) unless note.empty?
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+end
