d053cc3cab4f3f9d5491800cf40b9ada9b04a74b
kyle-poretto <kyleporetto@gmail.com>
2017-12-01 17:38:35 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+
   def index
     @songs = Song.all
   end
@@ -13,7 +14,7 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
+   
     if @song.save
       redirect_to @song
     else
@@ -47,7 +48,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
+
 end
 
