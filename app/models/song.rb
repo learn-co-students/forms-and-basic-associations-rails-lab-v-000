a7fa7875d0532bec4716fd6fd4dc1d6ea12b657e
alpbourne <alpbourne@gmail.com>
2017-12-12 21:59:35 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(notes)
     notes.each do |content|
       if content.strip != ''
-        self.notes.build(content :content)
+        self.notes.build(content: content)
       end
     end
   end
