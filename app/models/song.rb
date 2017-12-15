80a134e9dbee4911b094ebae3edcc418f9cc98b8
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-15 05:37:52 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,32 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def note_contents
+    self.notes.collect do |note|
+      note.content
+    end
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes.build(content: note) if note != ""
+    end
+  end
+end
