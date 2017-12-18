530b186fc96a40e2d5b112970f0018a54b1ce75d
Christian Egglin <cegglin@conncoll.edu>
2017-12-18 17:18:12 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,7 +14,7 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
+    self.genre = Genre.find_by(name: name)
   end
 
   def genre_name
