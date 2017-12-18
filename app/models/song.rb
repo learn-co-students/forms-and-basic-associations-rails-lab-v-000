c3c114ac93e860eb6e36f59240fdddbbf1826a24
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-12-18 23:56:50 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,39 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
+  
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def artist_name 
+    try(:artist).try(:name)
+    #binding.pry
+    #artist = Artist.find_or_create_by(name: name)
+    #self.artist = artist 
+  end
+  
+  def genre_name 
+    try(:genre).try(:name)
+  end
+  
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+  
+  def note_contents
+    self.notes.map(&:content)
+  end
+end 
 
