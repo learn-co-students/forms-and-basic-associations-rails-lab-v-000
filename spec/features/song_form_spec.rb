593ba364a94262c1898f5c3ff2cd6cb983226d5d
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-01-12 10:10:02 -0500

    Done.

--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -13,7 +13,9 @@ describe "the song form", :type => :feature do
     visit '/songs/new'
     fill_in :song_title, with: 'Little Earthquakes'
     fill_in :song_artist_name, with: 'Tori Amos'
+
     find('input[name="commit"]').click
+
     expect(Song.last.artist.name).to eq 'Tori Amos'
     expect(Artist.find_by(name: 'Tori Amos').songs.pluck(:title)).to include 'Little Earthquakes'
   end
