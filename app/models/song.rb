e8496fe5097a987fb5d7c4475eeb5d1a9504093f
Hannah Carnes <hecarnes@gmail.com>
2018-01-30 15:25:28 -0500

    create song can add artist by name

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,8 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
 
-  def artist_name
-     self.artist.name
+  def artist_name 
+    self.artist && self.artist.name
   end
 end
 
