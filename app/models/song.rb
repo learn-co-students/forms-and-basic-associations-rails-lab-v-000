25beba9124221b4c391ac4412289ecc14ea2f6cd
Richard Steinbach <steinbach.dick@gmail.com>
2018-02-01 09:00:36 -0800

    forms-and-basic-associations-rails-lab-v-000

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,39 @@
+#require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+
+  end
+
+  def artist_name
+
+    self.try(:artist).try(:name)
+  end
+
+  def note_contents=(contents)
+    contents.each do |c|
+      #binding.pry
+      note = Note.find_or_create_by(content: c)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    #binding.pry
+    self.notes.map(&:content).delete_if {|x| x == ""}
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+
+    self.try(:genre).try(:name)
+  end
+
+end
