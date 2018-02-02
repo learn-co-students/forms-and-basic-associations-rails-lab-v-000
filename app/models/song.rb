e5e39cb33c725b701e06838cdad3adc2221231e3
Jess Mitchell <jessicamitch42@gmail.com>
2018-02-02 21:27:28 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,30 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    artist = Artist.find_by(name: name)
+    self.artist = artist
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def genre_name=(name)
+    genre = Artist.find_by(name: name)
+    self.genre = genre
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  # def note_contents=(arr)
+  #   notes = Note.where(name:content)
+  # end
+  #
+  # def note_contents
+  #   self.note_contents
+  # end
+end
