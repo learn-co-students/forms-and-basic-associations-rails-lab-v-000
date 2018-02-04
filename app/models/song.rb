69dcf04c8b01819ecc5dd58acf253f2d4ed8f316
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-02-04 22:03:26 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+end
