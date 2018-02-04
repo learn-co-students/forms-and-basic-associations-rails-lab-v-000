adcfa773ef0a835dfc8c6580c13548ec10d49952
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-02-04 22:23:44 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -17,4 +17,16 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
 end
