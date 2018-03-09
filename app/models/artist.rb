498d17245cb5d6b27094194a3f48fd9f143d60bf
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-03-09 20:15:43 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  has_many :genres, through: :songs
+  
+  def song_ids=(ids)
+      ids.each do |id|
+        song = Song.find(id)
+        self.songs << song
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,29 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def artist_name
+    self.artist.name 
+  end
+  
+   def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+   end
+  
+  def genre_name
+    self.genre.name 
+  end
+  
+  def note_contents=(notes)
+    self.notes = Notes.find_or_create_by(name: content)
+  end
+  
+  def artist_name
+    self.artist.name 
+  end
 end
 
