cd73d8e2b47502b6c277c53073fc5f6e909a7c57
ingridwong0715 <ingrid830715@gmail.com>
2018-04-02 14:32:30 -0400

    Done.

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  describe 'genre_name=' do
-    it 'sets the genre to the genre of the specified name' do
-      song = Song.new
-      song.genre_name = 'Rock'
-      expect(song.genre.name).to eq 'Rock'
-    end
-  end
 
-  describe 'genre_name' do
-    it 'returns the genre name' do
-      song = Song.new
-      song.genre_name = 'Rock'
-      expect(song.genre_name).to eq 'Rock'
-    end
-  end
+  ### DELETE ###
+  # describe 'genre_name=' do
+  #   it 'sets the genre to the genre of the specified name' do
+  #     song = Song.new
+  #     song.genre_name = 'Rock'
+  #     expect(song.genre.name).to eq 'Rock'
+  #   end
+  # end
+  #
+  # describe 'genre_name' do
+  #   it 'returns the genre name' do
+  #     song = Song.new
+  #     song.genre_name = 'Rock'
+  #     expect(song.genre_name).to eq 'Rock'
+  #   end
+  # end
 
   describe 'artist_name=' do
     it 'sets the artist to the artist of the specified name' do
