59010bb4ae38621aad59a4953cecf67043bccb89
ronald vilorio <soccer23415@gmail.com>
2018-04-04 23:28:00 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,18 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  def artist_name
+    self.artist.name
+  end
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: id)
+  end
+  def genre_name
+    self.genre
+  end
+
+end
