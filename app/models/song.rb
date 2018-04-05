4b963b16a7920f77619875afff73b46fd90d0fb1
ronald vilorio <soccer23415@gmail.com>
2018-04-05 22:59:20 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,17 +3,36 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
   def artist_name
-    self.artist.name
+    if self.artist
+      self.artist.name
+    end
   end
+  def note_contents=(content)
+    content.each do |c|
+      note = Note.find_or_create_by(content: c)
+      if note.content && !note.content.empty?
+        self.notes << note
+      end
+    end
+  end
+  def note_contents
+      self.notes.collect do |note|
+        note.content
+      end
+  end
+
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: id)
+    self.genre = Genre.find_by(name: name)
   end
   def genre_name
-    self.genre
+    if self.genre
+      self.genre.name
+    end
   end
 
 end
