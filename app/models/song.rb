67fa161f144917142b3c5fef372ee04a4832999d
Katrina Brinson <kbrinso@gmail.com>
2018-04-15 00:51:44 +0000

    still working

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,28 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def note_contents=(text)
+    self.notes = Note.find_or_create_by(content: text)
+   end
+
+   def note_contents
+     self.notes.content
+   end
+
+   def genre_name=(name)
+     
+   end
+
+   def genre_name
+     self.genre.name
+   end
+
+   def aritst_name=(name)
+     self.artist = Artist.find_or_create_by(name: name)
+   end
+
+   def aritst_name
+     self.artist.name
+   end
+end
