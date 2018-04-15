25116c461f188821da519a3d6e4db3cd783b2213
Katrina Brinson <kbrinso@gmail.com>
2018-04-15 17:04:35 +0000

    8 failing

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,16 +3,29 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def note_contents=(text)
-    self.notes = Note.find_or_create_by(content: text)
+  def note_contents=(notes)
+    notes.each do |n|
+      if n != ""
+        note = Note.find_or_create_by(content: n)
+        self.notes << note
+      end
+    end
    end
 
    def note_contents
-     self.notes.content
+     allnotes = []
+    #  if self.notes.count == 1
+
+     self.notes.each do |t|
+       allnotes << t.content
+      #  t.content << allnotes
+     end
+     allnotes
+    # self.notes
    end
 
    def genre_name=(name)
-     
+
    end
 
    def genre_name
