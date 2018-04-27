460c0688f0a36507f764d3c366f4348555841626
Sean Oughton <seanoughton@gmail.com>
2018-04-27 23:13:42 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def song_artist_name=(name)
+    self.artist Artist.find_or_create_by(name: name)
+  end
+
+  def song_artist_name
+      self.artist.name
+  end
+
+end
