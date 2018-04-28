15c55bcfccf20ad9b7acffbea8714e610fcf557c
Yosa Yon <yosa.yon@gmail.com>
2018-04-28 20:54:34 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
   def create
    @song = Song.new(song_params)
    if @song.save
-    redirect_to @song
+    redirect_to song_path(@song)
    else
     render :new
    end
@@ -30,7 +30,7 @@ class SongsController < ApplicationController
     @song.update(song_params)
 
     if @song.save
-      redirect_to @song
+      redirect_to song_path(@song)
     else
       render :edit
     end
