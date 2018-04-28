08e54008887164c60d9bb39b087880d8aed747db
Tyler Maxwell <tmax818@mac.com>
2018-04-28 04:51:25 -0700

    init commit.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name if self.genre
+  end
+
+  def note_contents=(content)
+    content.each do |c|
+    self.notes.build(content: c) unless c.empty?
+    self.save
+  end
+  end
+
+  def note_contents
+    self.notes.collect {|n| n.content}
+  end
+
+
+end
