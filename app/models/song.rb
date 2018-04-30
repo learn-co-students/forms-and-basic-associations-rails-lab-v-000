4d0c7e6a0cb0bec04cdfa102943d8eaad2e15c0d
Sean Oughton <seanoughton@gmail.com>
2018-04-30 17:19:18 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,14 +6,20 @@ class Song < ActiveRecord::Base
 
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 
   def artist_name
+<<<<<<< HEAD
       if self.artist
         self.artist.name
       end
       #self.try(:artist).try(:name)
+=======
+      #self.artist.name
+      self.try(:artist).try(:name)
+>>>>>>> 2543803dadaa781351423c8f90784e6aa5408eec
   end
 
 
