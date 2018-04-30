5ed85263d032d5e4a9768327d39fc1b1504b4b01
Sean Oughton <seanoughton@gmail.com>
2018-04-30 19:45:24 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,9 +12,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    binding.pry
     @song = Song.new(song_params)
-    #@song.note_contents = (params[:song][:note_contents])
 
     if @song.save
       redirect_to @song
