e1b5dd9001ddfbcdcf07f9295b30b01a67105d15
Sean Oughton <seanoughton@gmail.com>
2018-04-30 17:45:47 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -17,5 +17,16 @@ class Song < ActiveRecord::Base
       #self.try(:artist).try(:name)
   end
 
+  def genre_id=(id)
+    genre = Genre.find_or_create_by(id: id)
+    self.genre = genre
+  end
+
+  def genre_id
+    if self.genre
+      self.genre.id
+    end
+  end
+
 
 end
