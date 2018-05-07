a60a7e20319a4d15e39e805a0458381ea276815d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-07 17:28:41 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+#we are building the custom setter method that allows songs to have many notes
+#take each id and find that note object and push into the song.notes collection
+  def note_ids=(ids)
+    ids.each do |id|
+      note = Note.find(id)
+      self.notes << note
+    end
+  end
+end
