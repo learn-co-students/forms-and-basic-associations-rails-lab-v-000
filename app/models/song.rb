75b23d68df4a0698a0977417c90c5e431f049d2d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-07 18:53:40 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,22 +5,24 @@ class Song < ActiveRecord::Base
 
 #we are building the custom setter method that allows songs to have many notes
 #take each id and find that note object and push into the song.notes collection
-  def note_contents=(contents)
-    contents.each do |content|
-      note = Note.find_by(content: content)
-      if note
-        self.notes << note
-      end
+  def note_contents=(notes)
+    notes.each do |content|
+      self.notes.build(content: content) if !content.empty?
     end
   end
 
+#map invokes the given block once for each element of self. so self has multiple
+#note elements. it calls .content on each and puts the values returned by the
+#block in a new array.
+
   def note_contents
-    @self.notes
+    @self.notes.map(&:content)
   end
 
 #we have a custom setter to assign an artist object to the song. It finds the artist object by name.
   def artist_name=(name)
     self.artist=Artist.find_or_create_by(name: name)
+    self.artist = artist
   end
 
 #we have a custom reader method to read the song's artists name.
@@ -29,5 +31,19 @@ class Song < ActiveRecord::Base
       self.artist.name
     end
   end
+
+#we have a custom setter to assign an artist object to the song. It finds the artist object by name.
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+#we have a custom reader method to read the song's artists name.
+  def genre_name
+    if self.genre.name
+      self.genre.name
+    end
+  end
+
 
  end
