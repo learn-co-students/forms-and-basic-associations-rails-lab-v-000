240494e5c6f7f95cba7097568822115bc6765f0c
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-05-11 00:14:35 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  def artist_name=(art_name)
+    self.artist = Artist.find_or_create_by(name: art_name)
+  end
+  
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+  
+  def genre_name=(gen_name)
+    self.genre = Genre.find_or_create_by(name: gen_name)
+  end
+  
+  def genre_name
+    self.try(:genre).try(:name)
+  end    
+  
+  def note_contents=(notes)
+    notes.each do |x|
+      if x.strip != ""
+        self.notes.build(content: x)
+      end
+    end
+  end
+  
+  def note_contents
+    self.notes.map { |x| x.content }
+  end  
+  
 end
 
