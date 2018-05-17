eb0b1c3ff5661b4904c57ee3c6b9ee1ff5139ec1
ElleeB <laura.trager@gmail.com>
2018-05-17 12:14:15 -0400

    Problem-solving collection_select for Song.genres

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,37 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    # self.artist.name
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+#   def note_contents=(contents)
+#     self.notes.each do |note|
+#       if note.contents
+#
+#
+#
+#
+#      = Genre.find_or_create_by(contents: contents)
+#   end
+#
+#   def note_contents
+#     self.note.contents
+#   end
+end
