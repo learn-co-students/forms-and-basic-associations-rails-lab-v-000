cd9a6d53e7ff8cacb361fc894ab9d2252cf94ae0
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-22 17:33:32 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,43 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def artist_name
+    unless self.artist == nil
+      self.artist.name
+    end
+  end
+
+  def note_contents=(array)
+    # binding.pry
+    array.each do |content|
+      self.notes.build(content: content) unless content == ""
+    end
+  end
+
+  def note_contents
+    self.notes.collect do |contents|
+      contents.content
+    end
+
+    # binding.pry
+  end
+
+end
