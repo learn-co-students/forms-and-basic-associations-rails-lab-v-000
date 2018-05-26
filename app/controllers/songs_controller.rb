616ae6b26e4252956d397d7aec9187160a0087d1
github username <github email address>
2018-05-26 13:26:41 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,5 @@
 class SongsController < ApplicationController
-  
-  
+
   def index
     @songs = Song.all
   end
@@ -49,7 +48,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
 
