a2e06fed4e6fad26f6dbfa0fd6788c81391a691c
Kaitlin Kelly <kk504408@gmail.com>
2018-05-28 18:58:32 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,37 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+#Artist
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+#Genre
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+#Notes
+  def note_contents=(contents)
+    contents.each do |content|
+      note = Note.find_or_create_by(content: content)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    self.notes ? self.notes.collect{|note| note.content}.reject(&:empty?) : nil
+  end
+
+end
