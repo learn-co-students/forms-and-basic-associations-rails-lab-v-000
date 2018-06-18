7404c7f58b30f559d1ee74ad0a7b40137949d80b
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-18 15:07:56 -0400

    Defined virtual attribute setter and getter for genre on Song

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(genre_name)
+    @genre = Genre.find_or_create_by(name: genre_name)
+
+    self.genre = @genre
+  end
+
+  def genre_name
+    self.genre.name if self.genre
+  end
 end
 
