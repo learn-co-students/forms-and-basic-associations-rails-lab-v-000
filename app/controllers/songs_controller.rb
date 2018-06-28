abae12e4f10988b1052228c33f28562cdaef7244
Purnima Lakshmi <codequeen1110@gmail.com>
2018-06-28 11:29:30 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+
 class SongsController < ApplicationController
   def index
     @songs = Song.all
@@ -9,9 +10,11 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    #binding.pry
   end
 
   def create
+    #binding.pry
     @song = Song.new(song_params)
 
     if @song.save
@@ -47,7 +50,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents:[])
   end
 end
 
