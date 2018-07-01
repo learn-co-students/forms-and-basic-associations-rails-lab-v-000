19308906f892403c8a38e20cc54b342a91e89d26
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-01 15:52:19 -0700

    models/song : genre_id/genre_name LoD methods

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,23 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_id=(genre_id)
+    @genre = Genre.find_or_create_by(id: genre_id)
+
+    self.genre = @genre
+  end
+
+  def genre_id
+    self.genre.id if self.genre
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+end
