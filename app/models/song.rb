ba6c75e0db4f46376764b26323fa8abd6dc1e824
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-03 15:03:01 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,26 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents(notes)
+    notes.each do |note|
+      self.notes << Note.find_or_create_by(content: note)
+    end
+  end
+end
