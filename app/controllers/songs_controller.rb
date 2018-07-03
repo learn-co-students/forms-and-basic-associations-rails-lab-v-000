5469fc9bed4b09c3dc57d22f6018ef9d5d296e88
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-03 16:05:50 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,6 +46,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_id, :note_content)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
