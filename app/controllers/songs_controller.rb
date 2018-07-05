d7836889a1c7a53460c07a2c3433099e645ca993
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-05 17:01:57 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,53 +1,29 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
-  end
-
-  def show
-    @song = Song.find(params[:id])
-  end
-
-  def new
-    @song = Song.new
-  end
-
-  def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
+      @songs = Song.all
     end
-  end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
+    def show
+      @song = Song.find(params[:id])
     end
-  end
 
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
+    def new
+      @song = Song.new
+    end
 
-  private
+    def edit
+      @song = Song.find(params[:id])
+    end
 
-  def song_params
-    params.require(:song).permit(:title)
-  end
+    def create
+      p song_params
+      song = Song.create(song_params)
+      redirect_to controller: 'songs', action: 'index'
+    end
+
+    private
+
+    def song_params
+      params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
+    end
 end
-
