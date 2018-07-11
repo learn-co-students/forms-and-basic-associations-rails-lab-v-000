44727ddf1795fed2a768a7c33f6742da4846b5b6
Karl Amoguis <ktamoguis@gmail.com>
2018-07-11 14:32:51 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    binding.pry
+    #binding.pry
     @song = Song.new(song_params)
 
     if @song.save
@@ -48,6 +48,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents:[])
+    params.require(:song).permit(:title, :artist_name, :genre_name, note_contents:[])
   end
 end
