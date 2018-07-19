1a797411ce253424fdb1339f9a6e91009b6bcbef
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 02:34:46 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,32 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name) #setter method #artist_name= is called whenever an Artist is initialized with a artist_name field
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+     self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+     self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(notes)
+    self.notes.each do |content|
+      if !content.empty?
+        self.notes.build(content: content)
+      end
+    end 
+  end
+
+  def note_contents
+  end
+
+end
