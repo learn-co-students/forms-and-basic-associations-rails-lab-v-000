cba27f551f31be13258c0111cb6fb45e5c466394
Victoria Meng <nights1ststar@protonmail.com>
2018-07-22 20:12:51 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,15 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
 
+  def notes=(content)
+    content.each do |c|
+      note = Note.find_or_create_by(content: c)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    self.notes ? self.notes.map(&:content) : nil
+  end
+
 end
