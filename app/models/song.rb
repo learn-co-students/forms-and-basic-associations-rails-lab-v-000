72fb5ec70ed5e797a6dee1da3f0da4bba940ff9d
arianaberger <ariana.berger@gmail.com>
2018-07-23 18:22:32 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,11 +11,24 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
+  # def genre_name=(name)
+  #   self.genre = Genre.find_or_create_by(name: name)
+  # end
+  #
+  # def genre_name
+  #   self.genre ? self.genre.name : nil
+  # end
+  #
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
   end
 
-  def genre_name
-    self.genre ? self.genre.name : nil
+  def note_contents
+    self.notes.map(&:content)
   end
+
 end
