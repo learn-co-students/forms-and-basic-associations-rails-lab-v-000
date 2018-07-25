390a6c3fa9c31a1c13ac3d45117c37a69f924936
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-25 04:41:55 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,22 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+     self.genre ? self.genre.name : nil
+  end
+
+  def song_artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def song_artist_name
+     self.artist ? self.artist.name : nil
+  end
+end
