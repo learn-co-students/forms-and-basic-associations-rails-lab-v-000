b58985ca05da970cca8e4da0bd06fa2521fed086
taryn yaeger <tsyaeger@gmail.com>
2018-07-28 18:47:07 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,11 +13,11 @@ class Song < ActiveRecord::Base
    end
    
    
-   def genre=(name)
+   def genre_name=(name)
      self.genre = Genre.find_or_create_by(name: name)
    end
  
-   def genre
+   def genre_name
       self.genre ? self.genre.name : nil
    end
    
