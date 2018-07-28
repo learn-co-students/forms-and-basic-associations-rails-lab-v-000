e30d355eaf501e2014300bf0283341c96fe28a5b
taryn yaeger <tsyaeger@gmail.com>
2018-07-28 18:42:07 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  
+   def artist_name=(name)
+     self.artist = Artist.find_or_create_by(name: name)
+   end
+ 
+   def artist_name
+      self.artist ? self.artist.name : nil
+   end
+   
+   
+   def genre=(name)
+     self.genre = Genre.find_or_create_by(name: name)
+   end
+ 
+   def genre
+      self.genre ? self.genre.name : nil
+   end
+   
+   
+   def note=(name)
+     self.note = Note.find_or_create_by(name: name)
+   end
+ 
+   def note_contents
+      self.note ? self.note.contents : nil
+   end
+
 end
 
