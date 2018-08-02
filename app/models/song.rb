f83c687a37fc75fb4ce6ed86c92296e0495f1475
ElvinGarcia <dimix181@gmail.com>
2018-08-02 16:56:59 -0400

    song model passing

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,39 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  attr_reader :genre_name
+  attr_reader :artist_name
+  #attr_reader :note_contents
+
+  def genre_name= (arg)
+    self.genre = Genre.all.find_or_create_by(name:arg)
+    self.save
+    @genre_name = arg if self.persisted?
+  end
+
+  def artist_name=(arg)
+    self.artist = Artist.all.find_or_create_by(name:arg)
+    self.save
+    @artist_name = arg if self.persisted?
+  end
+
+  def note_contents=(arg)
+    arg.each{|content|self.notes.build(content: content) unless content.empty? }
+   end
+   #  notes.each do |content|
+   #   if content.strip != ''
+   #     self.notes.build(content: content)
+   #   end
+   #
+   # end
+    # self.notes << Note.all.find_or_create_by(content:arg)
+    # self.save
+    # @note_contents = arg if self.persisted?
+
+  def note_contents
+     self.notes.collect(&:content)
+  end
+
+
+end
