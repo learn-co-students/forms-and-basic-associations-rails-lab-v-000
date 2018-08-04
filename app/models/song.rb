3d7a590a304fa8228dde8e4c65e2c7d9203f6c25
Elaine Anderson <eparie@bu.edu>
2018-08-04 14:58:35 +0000

    working

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+  self.artist = Artist.find_or_create_by(name: name)
 end
 
+def artist_name
+   self.artist ? self.artist.name : nil
+end
+
+def genre_name=(name)
+  self.genre = Genre.find_or_create_by(name: name)
+end
+
+def genre_name
+   self.genre ? self.genre.name : nil
+end
+
+def note_contents=(notes)
+  notes.each do |note|
+    if !note.empty?
+    self.notes.build(note)
+  end
+  end
+end
+
+def note_contents
+  self.notes
+end
+
+end
