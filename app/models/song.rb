9020548c67270568d251dc1e9358f36c33a48f21
github username <github email address>
2018-08-05 22:08:01 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+end
