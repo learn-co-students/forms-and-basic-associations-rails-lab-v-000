640fbe84f5303a185ead57eeb62f840224906bba
github username <github email address>
2018-08-05 23:30:50 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
@@ -11,4 +13,26 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
+  def note_contents=(notes)
+  notes.each do |content|
+    if content.strip != ''
+      self.notes.build(content: content)
+    end
+  end
+end
+
+def note_contents
+  self.notes.map(&:content)
+end
+
+def genre_name=(name)
+  genre = Genre.find_or_create_by(name: name)
+  self.genre = genre
+end
+
+def genre_name
+  self.try(:genre).try(:name)
+end
+
 end
