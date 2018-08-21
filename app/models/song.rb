f32d04f5137204782d5a2ee3a26d22d9c6a440f7
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-21 19:21:00 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,16 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+  
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+  
 end
 
