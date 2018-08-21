5b8f6583b0b7c25dcd08efc392081e2771f69890
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-21 19:26:18 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,5 +12,17 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+  
+  def note_contents=(content)
+    
+  end
+  
 end
 
