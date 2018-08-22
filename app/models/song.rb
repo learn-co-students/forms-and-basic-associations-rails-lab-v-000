7fb55f1a34fa148823b2b095c4eb2124302fdb36
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-22 01:53:05 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -28,7 +28,7 @@ class Song < ActiveRecord::Base
   
   def note_contents
     if self.notes
-      self.notes.compact.reject { |note| note.empty? }
+      self.notes.reject { |note| note.content.to_s.empty? }
     end
   end
   
