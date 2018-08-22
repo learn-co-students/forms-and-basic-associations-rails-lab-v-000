214a3caaa35f9405bf622e5a5e91be9e82b62c4f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-22 03:06:30 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,15 +20,30 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
   
+<<<<<<< HEAD
   def note_contents=(notes)
     notes.each do |content|
       self.notes << Note.find_or_create_by(content: content) unless content.strip.empty?
+=======
+  def note_contents=(contents)
+binding.pry
+    contents.each do |content|
+binding.pry
+      self.notes = Note.find_or_create_by(content: content)
+>>>>>>> 4391c05e6882b35650b1526c080210df95aa37ee
     end
   end
   
   def note_contents
+binding.pry
     if self.notes
+<<<<<<< HEAD
       self.notes.collect { |note| note.content unless note.content.to_s.strip.empty? }.compact
+=======
+      self.notes.content.collect { |content| 
+        binding.pry
+      }
+>>>>>>> 4391c05e6882b35650b1526c080210df95aa37ee
     end
   end
   
