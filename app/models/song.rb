cfc6ce033cb7ce3ee9d1ced2f49265045eba2a1d
Sequina Hall <sccaro89@gmail.com>
2018-09-08 15:56:46 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+  def artist_attributes=(artist)
+    self.artist = Artist.find_or_create_by(name: artist.name)
+    self.artist.update(artist)
+  end
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
