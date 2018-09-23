6b4ebe6ffef82eb6f37cca446195d1b4f829155c
fkmccallion <fkmccallion@gmail.com>
2018-09-22 20:07:43 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,11 +11,11 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  #def genre_id=(name)
-    #self.genre = Genre.find_or_create_by(name: name)
-  #end
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
 
-  #def genre_id
-    #self.genre ? self.genre.name : nil
-  #end
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
 end
