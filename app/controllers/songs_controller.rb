b2f7b09224b894e960d050f32e0eba5641dc24c6
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-26 23:27:15 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,7 @@ class SongsController < ApplicationController
   end
 
   def create
+
     @song = Song.new(song_params)
 
     if @song.save
@@ -48,6 +49,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_name)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
-  
+  has_many :notes
+
 
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
