e4007a123128e2007245bc62eef7364f598e8cad
Anu Khambete <anukhambete@gmail.com>
2018-10-09 22:20:27 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,6 +49,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name)
   end
 end
