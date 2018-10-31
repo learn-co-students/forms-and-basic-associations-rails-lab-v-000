9f80130da211f209954eae3a891084582afe8868
freeWillee <will.i.lee@outlook.com>
2018-10-31 13:01:05 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,7 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     if @song.save
+      binding.pry
       redirect_to @song
     else
       render :new
