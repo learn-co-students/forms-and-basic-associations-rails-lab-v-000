3f8a34d87fd46fd404348261d84377b4ad5327c7
mrjak13 <justinkenney13@gmail.com>
2018-12-13 11:04:44 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
 class Artist < ActiveRecord::Base
   # add associations here
+  has_many :songs
+
+  validates :name, presence: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  # validates :note_contents, presence: true
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(notes)    
+    notes.each do |note|
+      if note != ""
+        note = Note.find_or_create_by(content: note)
+        self.notes << note
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.collect {|note| note.content }
+  end
+  
 end
