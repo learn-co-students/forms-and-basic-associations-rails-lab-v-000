857a672e753f844184ae5fe2eab097b0809d902a
github username <github email address>
2019-01-16 10:40:36 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,14 @@
 class Artist < ActiveRecord::Base
   # add associations here
+  has_many :songs
+  has_many :genres, through: :songs
+
+  #How do we specifiy many different songs to an artist?<select>?
+    #Use array parameters with rails naming convention
+  def song_ids=(ids)
+    ids.each do |id|
+      song = Song.find(id)
+      self.songs << song
+    end
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'artists#index'
   resources :artists
   resources :songs
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
