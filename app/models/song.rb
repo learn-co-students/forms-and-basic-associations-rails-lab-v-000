4cdd5a3788b07c9d266023a5426d6c0659944c3e
Judy McGoldrick <jjeanius@verizon.net>
2019-01-19 20:24:01 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -19,4 +19,20 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
 
+  def note_contents=(contents)
+    contents.each do |content|
+      unless content==""
+      note = Note.create(content: content)
+      self.notes << note
+    end
+    end
+  end
+
+  def note_contents
+    self.notes.map do |note|
+    note.content
+    end
+  end
+
+
 end
