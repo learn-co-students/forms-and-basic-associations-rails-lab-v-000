23a4f4a4e1fc65e7876f4198e3b61ad93da9113d
Zachary Lehmann <zlehmann@gmail.com>
2019-01-23 23:31:00 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,4 +10,13 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
 end
