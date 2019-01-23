67e0f233348e61ce9f7beb659c6eec1d58837856
Zachary Lehmann <zlehmann@gmail.com>
2019-01-23 23:28:23 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,10 +44,11 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+
+
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name)
   end
 end
-
