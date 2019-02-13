c50cfeef5ac9616e46207e45028aa3037574bf23
Erin Green <erin.y.green@gmail.com>
2019-02-13 21:04:03 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,20 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  belongs_to :genre 
+  belongs_to :genre
+
+  def artist_name=(name)
+     self.artist = artist.find_or_create_by(name: name)
+   end
+
+   def artist_name
+      self.artist ? self.artist.name : nil
+   end
+
+   def genre=(name)
+      self.genre = genre.find_or_create_by(name: name)
+    end
+
+    def genre
+       self.genre ? self.genre.name : nil
+    end
 end
