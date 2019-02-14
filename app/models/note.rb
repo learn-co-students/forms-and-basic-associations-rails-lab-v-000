9aab8d4fd90a1050499f123fb05e7f8b87f3a3e4
Brad Schrag <brad.schrag@gmail.com>
2019-02-14 16:52:20 -0600

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,6 @@
 class Note < ActiveRecord::Base
   # add associations here
+  belongs_to :song
+
+  validates :content, presence: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,39 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+  	self.artist = Artist.find_or_create_by(:name => name)
+  	self.save
+  end
+
+  def artist_name
+  	self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+  	self.genre = Genre.find_or_create_by(:name => name)
+  	self.save
+  end
+
+  def genre_name
+  	self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(notes)
+  	notes.each do |note|
+  		new_note = Note.new(:content => note)
+  		if new_note.valid?
+  			self.notes << new_note
+  		end
+  	end
+  end
+
+  def note_contents
+  	self.notes.collect do |note|
+  		note.content
+  	end
+  end
 end
