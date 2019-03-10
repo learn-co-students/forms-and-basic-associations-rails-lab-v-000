ff6b1789da7b3ee260618d67e140c63a25b5d993
ricecake88 <vocalise@gmail.com>
2019-03-09 22:23:52 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,9 +11,8 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
-  def create
+  def create    
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -47,7 +46,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,48 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_id
+    self.genre ? self.genre.id : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |c|
+      note = Note.find_or_create_by(:content => c)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    contents = []
+    self.notes.each do |n|
+      if n.content != ""
+        contents << n.content
+      end
+    end
+    contents
+  end
+
 end
