c9deaca85dbc18ade5885a0e735b730a94786d6a
sammy-brake <sammy.brake@gmail.com>
2019-04-14 09:12:29 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,7 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
+    
   end
 
   def new
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,19 @@ class Song < ActiveRecord::Base
   belongs_to :genre 
   has_many :notes 
 
+  def genre_name=(genre)
+    self.genre = Genre.find_or_create_by(name: genre)
+  end 
 
-  def song_artist_name=(name)
+  def genre_name 
+    self.genre.name if self.genre 
+  end 
+
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end 
 
-  def song_artist_name
+  def artist_name
     self.artist ? self.artist.name : nil 
   end 
 end
