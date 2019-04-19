4152065cd1fc101187a2421575a0fd35cbacc4b2
sarawall1187 <sarawall1187@gmail.com>
2019-04-19 12:56:39 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,13 +9,15 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+   
   end
 
   def create
     @song = Song.new(song_params)
-
+   
     if @song.save
-      redirect_to song_path(@song)
+     
+      redirect_to @song
     else
       render :new
     end
