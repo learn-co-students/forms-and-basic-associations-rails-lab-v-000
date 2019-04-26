048afb2674cbefc58a7365c69b3edcbab0f3c2ce
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-25 22:50:55 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  # Define setters and getters
+
+  def note_contents=(ids)
+    ids.each do |id|
+      note = Note.find(id)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    self.notes
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
 end
