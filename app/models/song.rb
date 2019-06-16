5df192d16725453b139dee8184abdd0bb14473ed
guysbryant <guy.s.bryant@gmail.com>
2019-06-16 15:37:06 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      self.notes.build(content: content) if content.strip != ""
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+
 end
