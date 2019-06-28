3b58ab7e1342e6169a23f311ecd0b84bcec7ba15
christianavdm <christianavdm@gmail.com>
2019-06-28 13:56:48 -0700

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,8 @@
 class Genre < ActiveRecord::Base
   has_many :songs
+
+  def genre_name 
+    self.name
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,43 @@ class Song < ActiveRecord::Base
   def artist_name
      self.artist ? self.artist.name : nil
   end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+     self.genre ? self.genre.name : nil
+  end
+
+  def song_notes_1=(content)
+    self.notes << Note.find_or_create_by(content: content)
+  end
+
+  def song_notes_1
+     self.notes ? self.notes : nil
+  end
+
+  def song_notes_2=(content)
+    self.notes << Note.find_or_create_by(content: content)
+  end
+
+  def song_notes_2
+     self.notes ? self.notes : nil
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |note_content|
+      note = Note.create(content: note_content)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    note_contents = []
+    self.notes.each do |note|
+      note_contents << note.content
+    end
+    note_contents.reject(&:blank?)
+  end
 end
