02d7b1c0b95c6682f3341005961b74a88b30274a
Amil <github email address>
2019-09-30 17:37:10 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,49 @@
+require 'pry'
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      current_note = Note.find_or_create_by(content: note)
+      self.notes << current_note
+    end
+  end
+
+  def note_contents
+    # binding.pry
+    note_array = []
+    self.notes.map do |note|
+      if note.content != ""
+        note_array << note.content
+      end
+    end
+    note_array.uniq
+  end
 end
+
+#
+# <%= f.label :genre_name, list: "genres_autocomplete" %>
+  # <datalist id="genres_autocomplete">
+    # <% Genre.all.each do |genre| %>
+      # <option value="<%= genre.name %>">
+    # <% end %>
+  # </datalist> %>
