f6e45af984e8fea901717481f4fc3a7d69ecbc74
Allie Howard <alliemhow@gmail.com>
2019-11-19 03:23:31 -0500

    Done.

--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -20,9 +20,8 @@ describe "the song form", :type => :feature do
 
   it 'creates a song with a genre' do
     visit '/songs/new'
-    fill_in :song_artist_name, with: 'Tori Amos'
     fill_in :song_title, with: 'Little Earthquakes'
-    select 'Alternative', from: :song_genre_id
+    select 'Alternative', from: 'song_genre_id'
     find('input[name="commit"]').click
     expect(Song.last.genre.name).to eq 'Alternative'
     expect(Genre.find_by(name: 'Alternative').songs.pluck(:title)).to include 'Little Earthquakes'
@@ -31,9 +30,9 @@ describe "the song form", :type => :feature do
   it 'creates a song with notes' do
     visit '/songs/new'
     fill_in :song_title, with: 'Little Earthquakes'
-    fill_in :song_notes_attributes_0_content, with: 'great piano'
-    fill_in :song_notes_attributes_1_content, with: 'inaccurate seismology'
+    fill_in :song_notes_1, with: 'great piano'
+    fill_in :song_notes_2, with: 'inaccurate seismology'
     find('input[name="commit"]').click
-    expect(Song.last.notes.map(&:content)).to eq ['great piano', 'inaccurate seismology']
+    expect(Song.last.note_contents).to eq ['great piano', 'inaccurate seismology']
   end
 end
