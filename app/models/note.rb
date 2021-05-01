93ac346bb352998e4f4222c2cc6c607b6a8cf577
lrh2daizzo <herrera.lrh@gmail.com>
2021-04-30 19:35:12 -0500

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,3 @@
 class Note < ActiveRecord::Base
-  belongs_to :song 
+  belongs_to :song  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,35 +1,35 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist
+  belongs_to :artist 
   belongs_to :genre
-  has_many :notes
+  has_many :notes 
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
-    self.artist ? self.artist.name : nil
+    self.artist ? self.artist.name : nil  
   end
 
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
 
-  def genre_name
+  def genre_name 
     self.genre ? self.genre.name : nil 
   end
-  
+
   def notes_attributes=(contents)
-    contents.each do |content|
+    contents.each do |content| 
       if !content.empty?
         note = Note.create(content: content)
-        self.notes << note
+        self.notes << note 
       end
     end
   end
 
-  def notes_attributes 
+  def notes_attributes
     self.notes.map {|note| note.content}
   end
-
+  
 end
