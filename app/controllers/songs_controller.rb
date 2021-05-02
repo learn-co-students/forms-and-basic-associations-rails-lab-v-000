b36cc4d9b3efdb601023c2bfccdc25f0f9084d9f
Jesse Wenger <wengerj@gmail.com>
2021-05-02 14:22:49 +0000

    add artist_name setter getter

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
