83757cfa21d651ef762eb656566ab6eb4c4c2353
Jesse Wenger <wengerj@gmail.com>
2021-05-03 14:49:07 +0000

    add datalist

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,12 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def genre_id=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_id
+    self.genre ? self.genre.id : nil
+  end
 end
