25e671eb6be1309a876cc2d32a9eeb3968945ff3
Shasha Yan <shashayan@gmail.com>
2021-06-13 19:54:05 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,42 +12,40 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
-    end
+    song = Song.create(song_params)
+    redirect_to controller: "songs", action: "index"
+    # if @song.save
+    #   redirect_to @song
+    # else
+    #   render :new
+    # end
   end
 
   def edit
     @song = Song.find(params[:id])
   end
 
-  def update
-    @song = Song.find(params[:id])
+  # def update
+  #   @song = Song.find(params[:id])
+  #   @song.update(song_params)
+  #   if @song.save
+  #     redirect_to @song
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
+  # def destroy
+  #   @song = Song.find(params[:id])
+  #   @song.destroy
+  #   flash[:notice] = "Song deleted."
+  #   redirect_to songs_path
+  # end
 
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
 
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ActiveRecord::Base
   # add associations here
+  belongs_to :song 
 end
