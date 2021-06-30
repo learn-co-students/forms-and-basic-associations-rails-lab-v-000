a0f0b0f097d5fa85eb08c9a9aa8e59f5d978e98b
Matthew Williams <matthew.williams@flatironschool.com>
2021-06-29 19:26:07 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
-  # add associations here
+    belongs_to :artist
+    belongs_to :genre
+    has_many :notes
+
+    def artist_name=(name)
+      self.artist = Artist.find_or_create_by(name: name)
+    end 
+    def artist_name
+      self.artist ? self.artist.name : nil
+    end 
+
+    def genre_id=(id)
+        self.genre = Genre.find_or_create_by(id: id)
+    end 
+
+    def song_notes=(notes)
+      # binding.pry
+      notes.each do |note|
+        if note != ""  
+          self.notes.build(content: note) 
+        end 
+      end 
+    end 
 end
